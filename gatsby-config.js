be01cba89e305b69861f7d3a70843b4495b34394
Marco Emmanuel Noto <marcoemmanuelnoto@gmail.com>
2021-10-16 16:33:14 +0200

    progressive loading for heavy videos, not optimized for responsiviness yet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,39 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "and",
-  },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "and"
     },
-  ],
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp", {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/"
+            },
+            __key: "images"
+        }, {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "assets",
+                path: "./src/assets/"
+            },
+            __key: "assets"
+        }, {
+            resolve: "@chakra-ui/gatsby-plugin",
+            options: {
+                /**
+             * @property {boolean} [resetCSS=true]
+             * if false, this plugin will not use `<CSSReset />
+             */
+                resetCSS: true,
+                /**
+             * @property {boolean} [isUsingColorMode=true]
+             * if false, this plugin will not use <ColorModeProvider />
+             */
+                isUsingColorMode: true
+            }
+        }
+    ]
 };
---
+++ src/components/ProgressiveVideo.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react"
+import {Box} from "@chakra-ui/react"
+import "./ProgressiveVideo.css";
+
+const ProgressiveVideo = (props) => {
+    const [isVideoLoaded,
+        setIsVideoLoaded] = React.useState(false);
+
+    const handleVideoLoaded = () => {
+        setIsVideoLoaded(true);
+    };
+
+    return (
+        <Box position="relative" overflow="hidden" w="100vw" h="100vh">
+            <img
+                src={props.image}
+                className="video-thumb tiny"
+                style={{
+                opacity: isVideoLoaded
+                    ? 0
+                    : 1
+            }}/>
+            <video autoPlay playsInline muted loop>
+                <source
+                    src={props.video}
+                    type="video/mp4"
+                    onLoadedData={handleVideoLoaded}
+                    style={{
+                    opacity: isVideoLoaded
+                        ? 1
+                        : 0
+                }}/>
+            </video>
+        </Box>
+    )
+}
+
+export default ProgressiveVideo
