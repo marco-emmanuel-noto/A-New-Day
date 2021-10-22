0344497f8f0b097469971d8101b1d170bd622abf
Marco Emmanuel Noto <marcoemmanuelnoto@gmail.com>
2021-10-22 12:20:26 +0200

    debug step to check if the problem comes from muted not being added to the DOM node or on the onLoadedData event

--- src/components/ProgressiveVideo/ProgressiveVideo.js
+++ src/components/ProgressiveVideo/ProgressiveVideo.js
@@ -10,53 +10,26 @@ const ProgressiveVideo = (props) => {
         setIsVideoLoaded(true);
     };
 
-    useEffect(() => {
-        const video = document.getElementsByClassName("progressive-video")
-        console.log(video)
-        if (video[0].paused) {
-            handleVideoLoaded()
-            video.play()
-        }
-    });
+    // useEffect(() => {     const video =
+    // document.getElementsByClassName("progressive-video")     console.log(video)
+    //   if(video[0].paused) {         handleVideoLoaded()         video.play()
+    // } });
 
     return (
 
         <Box overflow="hidden" w="100vw" h="100vh" bg={props.bgColour}>
-            <img
+            {/* <img
                 src={props.image}
                 className="video-thumb tiny"
                 style={{
                 opacity: isVideoLoaded
                     ? 0
                     : 1
-            }}/>
+            }}/> */}
 
-            <div
-                dangerouslySetInnerHTML={{
-                __html: ` <video muted autoPlay playsInline loop className=${progressive-video}>
-                <source
-                    src=${props.video}
-                    type="video/mp4"
-                    onLoadedData=${handleVideoLoaded}
-                    style={{
-                    opacity: isVideoLoaded
-                        ? 1
-                        : 0
-                }}/>
-            </video>, `
-            }}></div>
-
-            {/* <video muted autoPlay playsInline loop className="progressive-video">
-                <source
-                    src={props.video}
-                    type="video/mp4"
-                    onLoadedData={handleVideoLoaded}
-                    style={{
-                    opacity: isVideoLoaded
-                        ? 1
-                        : 0
-                }}/>
-            </video> */}
+            <video muted autoPlay playsInline loop className="progressive-video">
+                <source src={props.video} type="video/mp4"/>
+            </video>
         </Box>
     )
 }
